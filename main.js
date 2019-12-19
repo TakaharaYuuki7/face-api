a602ddacd02bae0dd00ddb67f01042f6bd2501d9
Takaharayuuki7 <takaharayuuki7@gmail.com>
2019-12-19 23:55:36 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 const googlehome = require('google-home-notifier')
 const language = 'ja';
 
-//googlehome.device('teamd', language);
-googlehome.ip('192.168.43.147', language);
+//ここにGoogleフォームのipアドレスを記入
+googlehome.ip('', language);
 
+//Googleフォームに喋ってもらいたい内容を記入
 var voice_list = ["怒らないでください","ドンマイです","嫌な顔しないでください","今のは怖かったですね","楽しいですね","悲しいですか","驚きましたね"];
 
 for(var i = 0;i < process.argv.length; i++){
